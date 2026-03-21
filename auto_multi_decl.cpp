bf264d22e8fe40a9c8efdfcdd5fb2ec4125ea7e1
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-21 16:26:48 -0700

    auto_multi_decl.cpp

---
+++ auto_multi_decl.cpp
@@ -0,0 +1,19 @@
+/*
+ * Created by Durand Dyer-Branch on 3/21/26.
+ *
+ * This looks valid...
+ *
+ * 2 variables, 2 values
+ *
+ * But does it compile?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+#if 0
+    auto x = 50, y = 50.0;
+#endif
+    return 0;
+}
