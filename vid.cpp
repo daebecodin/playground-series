d07404ed717757e51987b427f2e3e79c991ef10a
daebecodin <duranddyer123@gmail.com>
2026-03-22 17:09:24 -0700

    vid.cpp

--- vid.cpp
+++ vid.cpp
@@ -7,16 +7,8 @@ using namespace std;
 
 int main()
 {
-    int x = 420, y = 67, z = 69;
+    auto x = 420, y = 67.0;
 
-    const int *p1 = &x; // reseat the pointer; not write through it
-    int *const p2 = &y; // modify through the pointer, not reseat the pointer
 
-    // *p1 = 42;
-    // p1 = &y;
 
-    // *p2 =42;
-    // p2 = &z;
-
-    return 0;
 }
